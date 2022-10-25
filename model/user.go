4d1cbb97a0aaae6cde1d5d2791f2f69170654193
Andika Bahari <andikabahari48@gmail.com>
2022-10-25 17:05:40 +0700

    Add register feature

--- model/user.go
+++ model/user.go
@@ -1,31 +1,14 @@
 package model
 
 import (
-	"database/sql/driver"
-
 	"gorm.io/gorm"
 )
-
-type role string
-
-const (
-	ORGANIZER role = "organizer"
-	CUSTOMER  role = "customer"
-)
-
-func (r *role) Scan(value any) error {
-	*r = role(value.([]byte))
-	return nil
-}
-
-func (r role) Value() (driver.Value, error) {
-	return string(r), nil
-}
 
 type User struct {
 	gorm.Model
 	Name     string
+	Username string `gorm:"index:,unique"`
 	Email    string
 	Password string
-	Role     role `sql:"type:ENUM('organizer', 'customer')"`
+	Role     string
 }
