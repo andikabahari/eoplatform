85c77b453b1f1f238186c17bf8e5a12baf8071d5
Andika Bahari <andikabahari48@gmail.com>
2022-10-30 14:20:57 +0700

    Setup Cloud Run config

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,34 @@ resource "google_storage_bucket" "ember" {
   location                    = var.region
   uniform_bucket_level_access = true
 }
+
+module "cloud_run" {
+  source  = "GoogleCloudPlatform/cloud-run/google"
+  version = "~> 0.2.0"
+
+  service_name = "eoplatform"
+  project_id   = var.project
+  location     = var.region
+  image        = "docker.io/andikabahari/eoplatform"
+
+  members = ["allUsers"]
+
+  template_annotations = {
+    "autoscaling.knative.dev/minScale" : 0,
+    "autoscaling.knative.dev/maxScale" : 2,
+    "generated-by" : "terraform",
+    "run.googleapis.com/client-name" : "terraform"
+  }
+
+  requests = {
+    "cpu" : "100m",
+    "memory" : "128Mi",
+  }
+
+  limits = {
+    "cpu" : "500m",
+    "memory" : "512Mi",
+  }
+
+  env_vars = var.env_vars
+}
