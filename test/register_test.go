cfc4e75952bda14863674f2e2722b21628cd8faf
Andika Bahari <andikabahari48@gmail.com>
2022-11-03 15:30:14 +0700

    Add register test case

--- test/register_test.go
+++ test/register_test.go
@@ -7,54 +7,71 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/andikabahari/eoplatform/request"
 	"github.com/andikabahari/eoplatform/server"
 	"github.com/andikabahari/eoplatform/server/handler"
 	"github.com/andikabahari/eoplatform/test/testhelper"
 	"github.com/stretchr/testify/suite"
 )
 
-type helloSuite struct {
+type registerSuite struct {
 	suite.Suite
 	mock    sqlmock.Sqlmock
 	server  *server.Server
-	handler *handler.HelloHandler
+	handler *handler.RegisterHandler
 }
 
-func (s *helloSuite) SetupSuite() {
+func (s *registerSuite) SetupSuite() {
 	os.Setenv("APP_ENV", "production")
 
 	var conn *sql.DB
 	conn, s.mock = testhelper.Mock()
 	s.server = testhelper.NewServer(conn)
-	s.handler = handler.NewHelloHandler(s.server)
+	s.handler = handler.NewRegisterHandler(s.server)
 }
 
-func TestHelloSuite(t *testing.T) {
-	suite.Run(t, new(helloSuite))
+func TestRegisterSuite(t *testing.T) {
+	suite.Run(t, new(registerSuite))
 }
 
-func (s *helloSuite) TestGreeting() {
+func (s *registerSuite) TestRegister() {
 	testCases := []struct {
 		Name         string
 		Endpoint     string
 		Method       string
-		Body         any
+		Body         *request.CreateUserRequest
 		ExpectedCode int
 	}{
 		{
-			"ok",
-			"/hello",
-			http.MethodGet,
+			"bad request",
+			"/v1/register",
+			http.MethodPost,
 			nil,
+			http.StatusBadRequest,
+		},
+		{
+			"ok",
+			"/v1/register",
+			http.MethodPost,
+			&request.CreateUserRequest{
+				Name:     "Organizer",
+				Username: "organizer",
+				Password: "password",
+				Role:     "organizer",
+			},
 			http.StatusOK,
 		},
 	}
 
 	for _, testCase := range testCases {
 		s.T().Run(testCase.Name, func(t *testing.T) {
+			query := regexp.QuoteMeta("SELECT * FROM `users` WHERE username = ? AND `users`.`deleted_at` IS NULL ORDER BY `users`.`id` LIMIT 1")
+			s.mock.ExpectQuery(query).WillReturnRows(s.mock.NewRows(nil))
+
 			bodyReader := new(bytes.Reader)
 			if testCase.Body != nil {
 				body, err := json.Marshal(testCase.Body)
@@ -67,7 +84,7 @@ func (s *helloSuite) TestGreeting() {
 			rec := httptest.NewRecorder()
 			ctx := s.server.Echo.NewContext(req, rec)
 
-			s.NoError(s.handler.Greeting(ctx))
+			s.NoError(s.handler.Register(ctx))
 			s.Equal(testCase.ExpectedCode, rec.Code)
 		})
 	}
