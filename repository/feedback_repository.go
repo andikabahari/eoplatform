0346f901ba6ec1056068cfdf6fd12532706c0bd3
Andika Bahari <andikabahari48@gmail.com>
2022-11-03 13:54:20 +0700

    Move query to repository

--- repository/feedback_repository.go
+++ repository/feedback_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"database/sql"
+
 	"github.com/andikabahari/eoplatform/model"
 	"gorm.io/gorm"
 )
@@ -8,6 +10,8 @@ import (
 type FeedbackRepository interface {
 	Get(feedbacks *model.Feedback, toUserID string)
 	Create(feedback *model.Feedback)
+	GetFeedbacksCount(fromUserID, toUserID any) int
+	GetOrdersCount(fromUserID, toUserID any) int
 }
 
 type feedbackRepository struct {
@@ -28,4 +32,36 @@ func (r *feedbackRepository) Get(feedbacks *[]model.Feedback, toUserID string) {
 
 func (r *feedbackRepository) Create(feedback *model.Feedback) {
 	r.db.Debug().Omit("FromUser").Omit("ToUser").Save(feedback)
+}
+
+func (r *feedbackRepository) GetFeedbacksCount(fromUserID, toUserID any) int {
+	feedbacksCount := 0
+
+	query := "SELECT COUNT(1) FROM feedbacks " +
+		"WHERE from_user_id=@FromUserID AND to_user_id=@ToUserID"
+
+	r.db.Debug().Raw(query,
+		sql.Named("FromUserID", fromUserID),
+		sql.Named("ToUserID", toUserID),
+	).Scan(&feedbacksCount)
+
+	return feedbacksCount
+}
+
+func (r *feedbackRepository) GetOrdersCount(fromUserID, toUserID any) int {
+	ordersCount := 0
+
+	query := "SELECT COUNT(1) FROM(" +
+		"SELECT DISTINCT o.id FROM orders o " +
+		"JOIN order_services os ON os.order_id=o.id " +
+		"JOIN services s ON s.id=os.service_id " +
+		"WHERE o.user_id=@FromUserID AND s.user_id=@ToUserID AND is_completed>0" +
+		") AS t"
+
+	r.db.Debug().Raw(query,
+		sql.Named("FromUserID", fromUserID),
+		sql.Named("ToUserID", toUserID),
+	).Scan(&ordersCount)
+
+	return ordersCount
 }
